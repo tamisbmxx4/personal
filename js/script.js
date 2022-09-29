858635de0206785b50962beb03d9c3cb40aabe46
mistafk <105982996+tamisbmxx4@users.noreply.github.com>
2022-09-30 01:18:52 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -103,7 +103,7 @@ var calculateAge = ()=>{
   let age_hours = document.querySelector('#meInTheWorld .hours');
   let age_minutes = document.querySelector('#meInTheWorld .minutes');
   let age_seconds = document.querySelector('#meInTheWorld .seconds');
-  let birth_date = new Date('2003, 07, 13');
+  let birth_date = new Date('07, 16, 2003);
   let years,months,days, hours, minutes, seconds;
   setInterval(function(){
     var current_date = new Date();
